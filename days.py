247bd88964406e96cd4792e6326927ad7b52b17c
Tiziana Ligorio <tligorio@hunter.cuny.edu>
2020-03-05 12:06:18 -0500

    program for assignment 38

---
+++ days.py
@@ -0,0 +1,36 @@
+#CSci 127 Teaching Staff
+#A program that uses functions to print out days.
+#Modified by:  ADD YOUR NAME HERE
+
+def dayString(dayNum):
+     """
+     Takes as input a number, dayNum, and
+     returns the corresponding day name as a string.
+     Example: dayString(1) returns "Monday".
+     Assumes that input is an integer ranging from 1 to 7
+     """
+     
+     dayString = ""
+
+     ###################################
+     ### FILL IN YOUR CODE HERE      ###
+     ### Other than your name above, ###
+     ### this is the only section    ###
+     ### you change in this program. ###
+     ###################################
+
+     return(dayString)
+
+
+
+def main():
+     n = int(input('Enter the number of the day: '))
+     dString = dayString(n)
+     print('The day is', dString)
+
+
+
+#Allow script to be run directly:
+if __name__ == "__main__":
+     main()
+                   
